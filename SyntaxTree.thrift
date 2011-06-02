67867314d4b5349b110e8c9592485401a4589e05
mapleelpam <mapleelpam@gmail.com>
2011-06-03 00:54:00 +0800

    fix typo error issue number #2

--- SyntaxTree.thrift
+++ SyntaxTree.thrift
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2011 Kai-Feng Chout - mapleellpam at gmail dot com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 
 namespace java tw.maple.generated
 namespace cpp tw.maple.generated
@@ -56,7 +72,7 @@ struct ClassDefinition
 struct MetaData
 {
     1: string id,
-    2: map<string/*key*/,string/*value*/>   kayvalues,
+    2: map<string/*key*/,string/*value*/>   keyvalues,
     3: list<string>                         values,
 }
         
