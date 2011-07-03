f4b880052d17f39035b7cfc1bb4f27c42ee0993c
Danty <danty.tw@gmail.com>
2011-07-03 18:58:20 +0800

    pnc/asc/thrift: should support "this" operator #66

--- SyntaxTree.thrift
+++ SyntaxTree.thrift
@@ -19,7 +19,7 @@ namespace java tw.maple.generated
 namespace cpp tw.maple.generated
 
 const string  PROTO_VERSION  = "0.0.1"   // human readable version
-const i64     PROTO_COUNTER  = 006   // pls add this counter when every time you release/push, and reset this counter when you add PROTO_VERsION
+const i64     PROTO_COUNTER  = 007   // pls add this counter when every time you release/push, and reset this counter when you add PROTO_VERsION
 
 enum ExpressionType
 {
@@ -170,6 +170,8 @@ service AstDumper
         oneway void literalNumberExpression( 1: Literal str ),
         oneway void literalBooleanExpression( 1: Literal str ),
         oneway void literalNull( ),
+
+        oneway void thisExpression( ),
     oneway void endExpressionList( ),
 
     oneway void addImport( 1: list<string> packages ),   // Do we need this? using namespace? or what ever suck?
