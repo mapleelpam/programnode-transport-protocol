87429adbbbeb730e4c4f20705ae53a829dc8712c
Danty <danty.tw@gmail.com>
2011-09-29 14:29:44 +0800

    add hasNext and Coerce

--- SyntaxTree.thrift
+++ SyntaxTree.thrift
@@ -19,7 +19,7 @@ namespace java tw.maple.generated
 namespace cpp tw.maple.generated
 
 const string  PROTO_VERSION  = "0.0.1"   // human readable version
-const i64     PROTO_COUNTER  = 025   // pls add this counter when every time you release/push, and reset this counter when you add PROTO_VERsION
+const i64     PROTO_COUNTER  = 026   // pls add this counter when every time you release/push, and reset this counter when you add PROTO_VERsION
 
 enum IdentifierType
 {
@@ -251,6 +251,9 @@ service AstDumper
 
 	oneway void registerNode( 1: i32 reg ),
 
-	oneway void startHasNext(  ),
-	oneway void endHasNext(),
+	oneway void startHasNext( ),
+	oneway void endHasNext( ),
+
+	oneway void startCoerce( ),
+	oneway void endCoerce( ),
 }
